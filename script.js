d5a78748586637a0410693413bc4f7c2c28f3ba3
Lalit Jamdagnee <110276457+lalit-jamdagnee@users.noreply.github.com>
2024-07-30 02:04:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showsidebar(){
+    let sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'block';
+}
+function hidesidebar(){
+    let sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
